492a6c92c28a66360d252bbfdf9f78ac873ed2ed
Crane Wen <cranewen@gmail.com>
2018-02-17 18:18:28 -0500

    Simple linear regression application v1.0. It allows you input a csv file, and then you can contral the training and test set ratio. It plots both training set and test set graph.

--- server.R
+++ server.R
@@ -1,23 +1,79 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(caTools)
+library(ggplot2)
 
-shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output, session) {
+  
+  getData <- function() {
+    reactive({
+      req(input$file)
+      df <- read.csv(file = input$file, header = TRUE)
+    })
+    return(df)
+  }
+  
+  selectDependentVar <- names(getData())
+  
+  updateSelectizeInput(session, inputId = "dv", choices = selectDependentVar, server = TRUE)
+  
+  df <- getData()
+  
+  # test <- function() { abc <- reactive({
+  #   input$train_test_ratio
+  # })
+  #   return(abc())
+  # }
+  
+  getSplitter <- function() {
+    splitter <- reactive( {
+      sample.split(df$Salary, SplitRatio = as.numeric(input$train_test_ratio))
+    })
+    return(splitter())
+  }
+  
+  
+  # getTrainingSet <- function() {
+  #   training_set <- eventReactive(input$plot_simple_lm, { subset(df, getSplitter() == TRUE) })
+  #   return(training_set())
+  # }
+  # 
+  # getTestSet <- function() {
+  #   test_set <- eventReactive(input$plot_simple_lm, { subset(df, getSplitter() == FALSE) })
+  #   return(test_set())
+  # }
+  
+  # training_set <- eventReactive(input$plot_simple_lm, { subset(df, getSplitter() == TRUE) })
+  # test_set <- eventReactive(input$plot_simple_lm, { subset(df, getSplitter() == FALSE) })
+  
+  
+  # regr <- lm(Salary ~ YearsExperience, training_set())
+  # y_pred <- predict(regr, newdata = test_set())
+  
+  # observeEvent(input$plot_simple_lm, {print(getTrainingSet())})
+  
+  observeEvent(input$plot_simple_lm, {
+    splitter <- getSplitter()
+    training_set <- subset(df, splitter == TRUE)
+    test_set <- subset(df, splitter == FALSE)
+    
+    print(training_set)
+    print(test_set)
+    
+    regr <- lm(Salary ~ YearsExperience, training_set)
+    y_pred <- predict(regr, newdata = test_set)
+    output$trainingPlot <- renderPlot(ggplot() + geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary), color = 'red') + 
+                                        geom_line(aes(x = training_set$YearsExperience, y = predict(regr, newdata = training_set)), color = 'blue') + 
+                                        ggtitle('Salary vs Experience (Training set)') + 
+                                        xlab('Years of Experience') + ylab('Salary'))
+    
+    output$testPlot <- renderPlot(
+      ggplot() + 
+        geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary), color = 'red') + 
+        geom_line(aes(x = training_set$YearsExperience, y = predict(regr, newdata = training_set)), color = 'blue') + 
+        ggtitle('Salary vs Experience (Test set)') + 
+        xlab('Years of Experience') + ylab('Salary')
+    )
   })
 
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,28 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  
+  titlePanel("Linear Regression"),
 
-  # Sidebar with a slider input for number of bins
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+      fileInput(inputId = "file", label = "Choose CSV File", 
+                 multiple = FALSE,
+                 accept = c("text/csv",
+                            "text/comma-separated-values,text/plain",
+                            ".csv")),
+      selectizeInput(inputId = "dv", label = "Please select the dependent variable",
+                     choices = NULL, options = list(create = FALSE)),
+      textInput(inputId = "train_test_ratio", label = "ratio", value = ''),
+      actionButton(inputId = "plot_simple_lm", label = "plotting")
     ),
 
-    # Show a plot of the generated distribution
+    
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("trainingPlot"),
+      plotOutput("testPlot")
     )
   )
 ))
